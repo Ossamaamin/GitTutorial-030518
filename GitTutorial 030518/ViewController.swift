c56476b10f0d7548e5bd32273a253d0e5ed29bc2
ossama amin <ossama.m.amin@gmail.com>
2018-05-03 23:32:54 +0200

    Initial Commit

---
+++ GitTutorial 030518/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTutorial 030518
+//
+//  Created by Admin on 5/3/18.
+//  Copyright © 2018 fourthpyramid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
